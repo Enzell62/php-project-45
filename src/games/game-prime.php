ca38eb663d111b52e9a08ffce01a76983f21fa81
enzell62 <enzell62@gmail.com>
2024-10-16 23:23:25 +0500

    step 9 comleted

---
+++ src/games/game-prime.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+function check_simple($n)
+{
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i == 0) {
+            return "no";
+        }
+    }
+    return "yes";
+}
+
+function games_brain_prime()
+{
+    $a = rand(2, 100);
+    $rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    $question = "Question: {$a}" . "\n" . 'Your answer';
+    $answer = check_simple($a);
+    return [$rules, $question, $answer];
+}
--- src/games/game-progression.php
+++ src/games/game-progression.php
@@ -5,19 +5,13 @@ namespace BrainGames\Games\Progression;
 function games_brain_progression()
 {
     $number_of_elements = rand(5, 10);
-    // echo("number_of_elements: " . $number_of_elements . "\n"); // ________________
     $step = rand(2, 5);
     $start = rand(0, 100 - ($number_of_elements * $step));
-    // echo("start: " . $start . "\n"); // ________________
     $hidden_element = rand(1, $number_of_elements);
-    // echo("hidden_element: " . $hidden_element . "\n"); // ________________
     $elements = [$start];
     for ($i = 1; $i < $number_of_elements; $i++) {
         $elements[] = $elements[array_key_last($elements)] + $step;
     }
-    // print_r("elements: "); // ________________
-    // print_r($elements); // ________________
-    // print_r("\n"); // ________________
     $answer = $elements[$hidden_element - 1];
     $elements[$hidden_element - 1] = "..";
     $string_of_elements = implode(" ", $elements);
@@ -25,5 +19,3 @@ function games_brain_progression()
     $rules = 'What number is missing in the progression?';
     return [$rules, $question, $answer];
 }
-
-
