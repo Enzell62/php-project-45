c6ee3934ad0ed2f391087924e89d906af6403379
enzell62 <enzell62@gmail.com>
2024-10-19 12:02:11 +0500

    fix

--- src/games/game-even.php
+++ src/games/game-even.php
@@ -6,7 +6,7 @@ function games_brain_even()
 {
     $number = rand(1, 100);
     $answer = $number % 2 == 0 ? "yes" : "no";
-    $rules = 'Answer "yes" if the number is even, otherwise answer "no"';
+    $rules = 'Answer "yes" if the number is even, otherwise answer "no".';
     $question = "Question: {$number}" . "\n" . 'Your answer';
     return [$rules, $question, $answer];
 }
